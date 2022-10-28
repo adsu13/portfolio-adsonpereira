85f8a7a6aa3f8f0ae84ac1e6245514f0191b122c
Adson Pereira <57012550+adsu13@users.noreply.github.com>
2022-10-28 15:10:43 -0300

    Scroll suave nos conteudos adicionado
    Implementado o Scroll suave do redirecionamento de conteudos do site.

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$('nav a').click(function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        menuHeight = $('nav').innerHeight(),
+        targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 500);
+});
+document.querySelector('#menu-btn').addEventListener('click', function() {
+    //console.log("Menu");
+
+    document.querySelector('#menu-site').classList.toggle("active");
+    document.querySelector('#menu-icon').classList.toggle("active");
+});
