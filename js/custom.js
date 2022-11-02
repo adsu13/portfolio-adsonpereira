cc2dd54ad0797d382a3e03528e52450d8357ae55
Adson Pereira <57012550+adsu13@users.noreply.github.com>
2022-11-02 14:01:58 -0300

    HOVER DAS HABILIDADES
    - FOI IMPLEMENTADO O HOVER MOSTRANDO A DESCRIÇÃO DAS HABILIDADES
    
    -FOI IMPLEMENTADO O BOTÃO PARA BAIXAR O CURRÍCULO
    
    - ALTERAÇÃO NO FOOTER

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ $('nav a').click(function(e) {
     }, 500);
 });
 document.querySelector('#menu-btn').addEventListener('click', function() {
-    //console.log("Menu");
+
 
     document.querySelector('#menu-site').classList.toggle("active");
     document.querySelector('#menu-icon').classList.toggle("active");
